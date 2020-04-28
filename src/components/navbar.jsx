50fbf1c0978e044fdcdd129dda154193f98e762e
Anupama Dissanayake <anu2006developer@gmail.com>
2020-04-28 15:05:43 +0530

    navbar recreated 😊

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,46 +1,39 @@
 import React, { Component } from "react";
-import { ReactComponent as Arrow } from "../svgs/arrow.svg";
-import { ReactComponent as News } from "../svgs/news.svg";
-import { ReactComponent as Home } from "../svgs/home.svg";
-import { ReactComponent as Gallery } from "../svgs/gallery.svg";
-import { ReactComponent as Contact } from "../svgs/contact.svg";
+
+import { ReactComponent as Chakra } from "../svgs/chakra.svg";
+
+class Navitem extends Component {
+  render() {
+    return (
+      <li className="nav-item">
+        <a href="/" className="nav-link">
+          {this.contentOrText()}
+        </a>
+      </li>
+    );
+  }
+  contentOrText() {
+    const { text, children } = this.props;
+    return text ? <span className="nav-text">{text}</span> : children;
+  }
+}
 
 class Navbar extends Component {
   state = {};
   render() {
     return (
-      <React.Fragment>
-        <div className="arrow-container">
-          <span id="menu-title">KITHULGODARANYA</span>
-          <Arrow className="svg arrow" />
-        </div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="../../public/index.html">
-              <Home className="svg nav-svg" />
-              <span className="nav-text">Home</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="../../public/index.html">
-              <Gallery className="svg nav-svg" />
-              <span className="nav-text">Gallery</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="../../public/index.html">
-              <News className="svg nav-svg" />
-              <span className="nav-text">News and Events</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="../../public/index.html">
-              <Contact className="svg nav-svg" />
-              <span className="nav-text">Contact Us</span>
-            </a>
-          </li>
-        </ul>
-      </React.Fragment>
+      <ul>
+        <Navitem>
+          <div className="title-container">
+            <span className="title">Kithulgoda Aranya</span>
+            <Chakra />
+          </div>
+        </Navitem>
+        <Navitem text="Home" />
+        <Navitem text="Gallery" />
+        <Navitem text="News" />
+        <Navitem text="Contacts" />
+      </ul>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import Navbar from "./components/navbar";
-import "./components/navbar.css";
-import TopBar from "./components/topbar";
+import "./components/navbar2.css";
+import "./components/topbar.css";
+// import TopBar from "./components/topbar";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<Navbar />, document.querySelector("nav"));
-ReactDOM.render(<TopBar />, document.querySelector("main"));
+ReactDOM.render( < Navbar / > , document.querySelector("nav"));
+// ReactDOM.render(<TopBar />, document.querySelector("main"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
